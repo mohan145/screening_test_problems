e45f76b795769075003ad34433730a4c2168b056
mohan145 <mohan145krsihna@gmail.com>
2020-02-26 22:01:41 +0530

    Implemented Solution for Redundant Braces

--- redundant_braces.py
+++ redundant_braces.py
@@ -1,19 +1,50 @@
 class Stack():
-
+    
     def __init__(self):
-        self.stack = []
-        self.top = -1
-
+        self.stack=[]
+        self.top=-1
+    
     def pop(self):
-        if self.top == -1:
+        if self.top==-1:
             return "Stack Empty"
-        top_element = self.stack[self.top]
-        self.top -= 1
+        top_element=self.stack[self.top]
+        self.top-=1
         self.stack.pop()
         return top_element
-
-    def push(self, element):
-        self.top += 1
+    
+    def push(self,element):
+        self.top+=1
         self.stack.append(element)
 
-
+class Solution:
+    # @param A : string
+    # @return an integer
+    def braces(self, A):
+      
+        stack = Stack()  
+        
+        def isCloseBrace(char):
+            if char==")":
+                return True
+            return False
+        
+        def isOperator(element):
+            if element=="+" or element=="-" or element=="*" or element=="/":
+                return True
+            return False
+  
+        for element in A:  
+            if isCloseBrace(element):  
+                poped_element = stack.pop()
+                redundant = True
+                while (poped_element != '('):  
+                    if isOperator(poped_element):  
+                        redundant = False
+      
+                    poped_element = stack.pop()
+      
+                if redundant: 
+                    return 1
+            else: 
+                stack.push(element) 
+        return 0
